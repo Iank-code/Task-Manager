4fcd78c44562f6bdad6082564b1012c3de75f36e
ianK-code <kian99564@gmail.com>
2023-02-27 22:43:02 +0300

    Done with custom fetch hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,27 @@
+import FetchHook from "./hooks/Fetch.js";
+import React, { useState } from "react";
+
 function Home() {
+  const [url, setUrl] = useState("https://jsonplaceholder.typicode.com/posts");
+
+  const { data, isLoading, error } = FetchHook(url);
+  const handleButtonClick = () => {
+    setUrl("https://jsonplaceholder.typicode.com/comments");
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div className="Home">
-      <p>THis is the home page</p>
+    <div>
+      <ul>{data && data.map((item) => <li key={item.id}>{item.title}</li>)}</ul>
+      <button onClick={handleButtonClick}>Load Comments</button>
     </div>
   );
 }
-
 export default Home;
---
+++ src/components/hooks/Fetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function Fetch(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err.message);
+        setError(err);
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return { data, isLoading, error };
+}
+
+export default Fetch;
