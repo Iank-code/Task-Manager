36665d5b5fb19db6db6715deeee109ec493aeb20
ianK-code <kian99564@gmail.com>
2023-03-02 21:09:47 +0300

    Fixed a bug inside the UsePatch hook

--- src/components/UpdateTask.js
+++ src/components/UpdateTask.js
@@ -1,3 +1,57 @@
+// import React, { useState } from "react";
+// import UsePatch from "./hooks/UsePatch";
+
+// function UpdateTask({ id }) {
+//   const [name, setName] = useState("");
+//   const [description, setDescription] = useState("");
+
+//   const body = {
+//     name: name,
+//     description: description,
+//   };
+//   const { error } = UsePatch(
+//     `http://localhost:9292/patch/${id}`,
+//     body
+//   );
+//   const submitChanges = (e) => {
+//     e.preventDefault();
+
+//     if(error){
+//       console.log(error)
+//     }
+//     window.location.reload();
+//   };
+//   return (
+//     <>
+//       <form onSubmit={submitChanges}>
+//         <label>
+//           Set Name:{" "}
+//           <input
+//             type="text"
+//             placeholder="Name"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//           />
+//         </label>
+
+//         <label>
+//           Set Description:{" "}
+//           <input
+//             type="text"
+//             placeholder="Description"
+//             value={description}
+//             onChange={(e) => setDescription(e.target.value)}
+//           />
+//         </label>
+
+//         <button type="submit">Save</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default UpdateTask;
+
 import React, { useState } from "react";
 import UsePatch from "./hooks/UsePatch";
 
@@ -5,22 +59,26 @@ function UpdateTask({ id }) {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
 
-  const body = {
-    name: name,
-    description: description,
-  };
+  const { error, patchResponse, patch } = UsePatch(
+    `http://localhost:9292/patch/${id}`
+  );
+
   const submitChanges = (e) => {
     e.preventDefault();
-    const { error } = UsePatch(
-      `http://localhost:9292/patch/${id}`,
-      body
-    );
-
-    if(error){
-      console.log(error)
-    }
-    window.location.reload();
+    patch({
+      name: name,
+      description: description,
+    });
   };
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (patchResponse) {
+    window.location.reload();
+  }
+
   return (
     <>
       <form onSubmit={submitChanges}>
--- src/components/hooks/UsePatch.js
+++ src/components/hooks/UsePatch.js
@@ -1,9 +1,40 @@
+// import { useState, useEffect } from "react";
+
+// function usePatch(url, body) {
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     fetch(url, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(body),
+//     })
+//       .then((r) => {
+//         if (!r.ok) {
+//           throw new Error(r.status);
+//         }
+//         return r.json();
+//       })
+//       .catch((err) => {
+//         console.error(err.message);
+//         setError(err);
+//       });
+//   }, [url,body]);
+
+//   return { error };
+// }
+
+// export default usePatch;
+
 import { useState, useEffect } from "react";
 
-function usePatch(url, body) {
+function usePatch(url) {
   const [error, setError] = useState(null);
+  const [patchResponse, setPatchResponse] = useState(null);
 
-  useEffect(() => {
+  const patch = (body) => {
     fetch(url, {
       method: "PATCH",
       headers: {
@@ -17,13 +48,21 @@ function usePatch(url, body) {
         }
         return r.json();
       })
+      .then((data) => {
+        setPatchResponse(data);
+      })
       .catch((err) => {
         console.error(err.message);
         setError(err);
       });
-  }, [url,body]);
+  };
 
-  return { error };
+  useEffect(() => {
+    setPatchResponse(null);
+    setError(null);
+  }, [url]);
+
+  return { error, patchResponse, patch };
 }
 
 export default usePatch;
